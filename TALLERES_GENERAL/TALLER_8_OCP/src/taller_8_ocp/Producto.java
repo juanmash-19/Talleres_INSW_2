1af735e9272fe17e2dedb691c518fab5f338cdc5
JuanMartin <juanmasuhe1909@gmail.com>
2024-09-16 08:16:11 -0500

    Subiendo talleres general #8 principios

---
+++ TALLERES_GENERAL/TALLER_8_OCP/src/taller_8_ocp/Producto.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller_8_ocp;
+
+/**
+ *
+ * @author juanm
+ */
+public class Producto {
+    private String nombre;
+    private int stock;
+    private String categoria;
+
+    public Producto(String nombre, int stock, String categoria) {
+        this.nombre = nombre;
+        this.stock = stock;
+        this.categoria = categoria;
+    }
+}
